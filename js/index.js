f3ac600aaa6a5b91b41d5c7cc3d458cd04c26046
Anna Bezrukova <annelia_55@mail.ru>
2017-04-26 19:11:23 +0800

    parallax siluet and model

--- js/index.js
+++ js/index.js
@@ -1,3 +1,4 @@
+// Paarallax header
 $(window).scroll(function(){
 
     var wScroll = $(this).scrollTop(); // this = window. It tells how far it is scrolled from the top
@@ -5,4 +6,16 @@ $(window).scroll(function(){
     $('.logo').css({
        'transform' : 'translate(0px, '+ wScroll /2 +'%)'
     });
+
+    $('.siluet').css({
+        'transform' : 'translate(0px, '+ wScroll /4 +'%)'
+    });
+
+    $('.model').css({
+        'transform' : 'translate(0px, -'+ wScroll /16 +'%)' //with "-" the object move UP while scrolling
+    });
+
 });
+
+
+// Stop parallax at certain point
