c0714c1707b22a572466d1c68cc82b166513753e
Anna Bezrukova <annelia_55@mail.ru>
2017-05-01 13:10:20 +0800

    hover over gallery images - appearing text

--- js/index.js
+++ js/index.js
@@ -20,17 +20,10 @@ $(window).scroll(function(){
     });
 
     /* Gallery */
-    //if(wScroll > $('.gallery').offset().top) {
-    //    $('.gallery img').each(function(){                  //show each image while scrolling
-    //
-    //        $('.gallery img').addClass('is-showing');
-    //    });
-    //}
-
-    if(wScroll > $('.gallery').offset().top - ($(window).height() / 1.5)){
-    $('.gallery img').each(function(i){               //show each image while scrolling
-        setTimeout(function(){                    //(i) => means each one
-            $('.gallery img').eq(i).addClass('is-showing');  //eq() => means look at the index and find the right one
+    if(wScroll > $('.gallery').offset().top - ($(window).height() / 2.2)){
+    $('.gallery figure').each(function(i){               //show each image while scrolling
+        setTimeout(function(){                       //(i) => means each one
+            $('.gallery figure').eq(i).addClass('is-showing');  //eq() => means look at the index and find the right one
         }, 250 * (i + 1));                                             //set rge time interval
     });
 }
